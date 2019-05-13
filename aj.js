0a81edfcc2e3fdf32807e83c36f717e7799047d8
srikanthacharya <42923978+srikanthacharya@users.noreply.github.com>
2019-05-13 17:34:30 +0530

    Create aj.js

---
+++ aj.js
@@ -0,0 +1,25 @@
+<div ng-app="myApp" ng-controller="myCtrl">
+    <h1 ng-click="changeName()">{{firstname}}</h1>
+</div>
+
+<script>
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.firstname = "John";
+    $scope.changeName = function() {
+        $scope.firstname = "Nelly";
+    }
+});
+/*the above example is for the binding a different data and using a controller to show the dat*/
+
+<style>
+input.ng-invalid {
+    background-color: lightblue;
+}
+</style>
+<body>
+
+<form ng-app="" name="myForm">
+    Enter your name:
+    <input name="myName" ng-model="myText" required>
+</form>
